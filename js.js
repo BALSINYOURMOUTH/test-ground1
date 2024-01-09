5845c263db7740315185c208c3806ed86740fa2a
laksura <133021017+BALSINYOURMOUTH@users.noreply.github.com>
2024-01-09 01:54:15 +0000

    fixed the image positioning issue abd the menu

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll ('#btn');
+const buttons = document.querySelectorAll ('.btn');
 
 buttons.forEach(button => {
     button.addEventListener('click', function() {
